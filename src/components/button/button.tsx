34daa1f74cfa5cfaaecfda07dce9271a43438878
Patryk Makarewicz <makarewicz.szymon@gmail.com>
2023-09-11 22:13:40 +0200

    Clean styles

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -5,5 +5,5 @@ type CustomButtonProps = {
 export type ButtonProps = CustomButtonProps & React.HTMLProps<HTMLButtonElement>;
 
 export const Button = ({ label }: ButtonProps) => {
-  return <button style={{ backgroundColor: 'blue', padding: '10px 20px', borderRadius: '4px' }}>{label}</button>;
+  return <button style={{ backgroundColor: 'yellow', padding: '10px 20px', borderRadius: '4px' }}>{label}</button>;
 };
