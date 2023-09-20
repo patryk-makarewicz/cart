c658722ec1bc3890e0aa9926e627f0ff9ebf2eec
Patryk Makarewicz <makarewicz.szymon@gmail.com>
2023-09-21 00:28:26 +0200

    Removed header styles file and add tailwind css

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,14 +2,14 @@ import Image from 'next/image';
 import Logo from '../../assets/logo_black.svg';
 import FlagPl from '../../assets/lang_pl.png';
 import FlagEn from '../../assets/lang_en.png';
-import * as Styled from './header.styles';
+
 import Link from 'next/link';
 
 const Header = ({ lng }: { lng: string }) => (
-  <Styled.Wrapper>
-    <Styled.InnerWrapper>
+  <div className="border-b border-gray-200 bg-white">
+    <div className="mx-auto flex max-w-screen-2xl items-center justify-between p-2.5">
       <Image priority src={Logo} width={30} height={36} alt="Logo makaDev" />
-      <Styled.FlagContainer>
+      <div className="h-8 w-8 rounded-full border-2 border-white drop-shadow-lg">
         {lng === 'pl' ? (
           <Link href={'/en'}>
             <Image priority src={FlagPl} width={30} height={30} alt="Poland flag" />
@@ -19,9 +19,9 @@ const Header = ({ lng }: { lng: string }) => (
             <Image priority src={FlagEn} width={30} height={30} alt="English flag" />
           </Link>
         )}
-      </Styled.FlagContainer>
-    </Styled.InnerWrapper>
-  </Styled.Wrapper>
+      </div>
+    </div>
+  </div>
 );
 
 export default Header;
