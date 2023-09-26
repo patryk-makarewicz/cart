77b1185790d84f60b9828d8e8c9b789201b2dc14
Patryk Makarewicz <makarewicz.szymon@gmail.com>
2023-09-26 22:55:05 +0200

    Add redux, config and model

---
+++ src/api/artworks/artworks.model.ts
@@ -0,0 +1,18 @@
+export type ArtworksListModel = {
+  records: {
+    id: string;
+    createdTime: string;
+    fields: {
+      imageAlt: string;
+      price: number;
+      imageSrc: string;
+      category: string;
+      bestseller: string;
+      featured: string;
+      details: string;
+      name: string;
+      recommendations: string;
+      currency: string;
+    };
+  }[];
+};
