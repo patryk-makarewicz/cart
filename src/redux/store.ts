79099eac3e9339b6ea42cd307f84008d93bdef28
Patryk Makarewicz <makarewicz.szymon@gmail.com>
2023-09-28 12:47:58 +0200

    Remove counterSlice

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,12 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from './features/counterSlice';
 import cartReducer from './features/cartSlice';
 import { artworksApi } from './services/artworksApi';
 import { setupListeners } from '@reduxjs/toolkit/dist/query';
 
 export const store = configureStore({
   reducer: {
-    counterReducer,
     cartReducer,
     [artworksApi.reducerPath]: artworksApi.reducer
   },
