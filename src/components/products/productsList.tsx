f78a35c72be9c281fed3f32a621c0406d1a9fcad
Patryk Makarewicz <makarewicz.szymon@gmail.com>
2023-09-28 12:38:23 +0200

    Create basic ProductList component

---
+++ src/components/products/productsList.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import { ArtworksListModel } from '@/api/artworks/artworks.model';
+import { useAppDispatch } from '@/redux/hooks';
+import { addToCart, resetCart } from '@/redux/features/cartSlice';
+
+export const ProductsList = ({ records }: ArtworksListModel) => {
+  const dispatch = useAppDispatch();
+  if (!records) {
+    return <p>No data</p>;
+  }
+
+  return (
+    <div>
+      <ul>
+        {records.map((item) => (
+          <li key={item.id}>
+            {item.fields.name} <button onClick={() => dispatch(addToCart(item))}>+</button>
+          </li>
+        ))}
+      </ul>
+      <button onClick={() => dispatch(resetCart())}>reset cart</button>
+    </div>
+  );
+};
