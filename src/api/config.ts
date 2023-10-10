47a8c6aad23ecbf8ed73f2caa1dc7478c10f50a3
Patryk Makarewicz <makarewicz.szymon@gmail.com>
2023-10-10 10:00:56 +0200

    Add NEXT_PUBLIC to header and base url

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,7 +1,7 @@
 export const useAPImocks = false;
 
-export const BASE_URL = `https://api.airtable.com/v0/${process.env.DB_ID}`;
+export const BASE_URL = `https://api.airtable.com/v0/${process.env.NEXT_PUBLIC_DB_ID}`;
 
 export const headers = {
-  Authorization: `Bearer ${process.env.API_KEY}`
+  Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_KEY}`
 };
