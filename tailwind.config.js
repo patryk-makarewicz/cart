5382900b811a855007875baaf6ff6d2ccb6c21f0
Patryk Makarewicz <makarewicz.szymon@gmail.com>
2023-10-16 10:50:41 +0200

    Create custom animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,15 @@ module.exports = {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+      },
+      keyframes: {
+        fadeIn: {
+          from: { opacity: 0 },
+          to: { opacity: 1 }
+        }
+      },
+      animation: {
+        fadeIn: 'fadeIn 0.5s'
       }
     }
   },
