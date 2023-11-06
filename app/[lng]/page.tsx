08c30db72a3df748fb5cd1f700a5f62ea45bfa3a
Patryk Makarewicz <makarewicz.szymon@gmail.com>
2023-11-06 11:26:19 +0100

    Add background to home tab

--- app/[lng]/page.tsx
+++ app/[lng]/page.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 
 import { PageWrapper } from '@/[lng]/pageWrapper';
 
+import bg from '@/assets/bg_home.jpeg';
 import Logo from '@/assets/logo_makaDev.png';
 import { Button } from '@/components/button';
 import { Chatbot } from '@/components/chatbot';
@@ -20,8 +21,15 @@ const Home = async ({ params: { lng } }: lngProps) => {
   return (
     <>
       <PageWrapper>
-        <div className="m-auto flex flex-col items-center">
-          <div className="max-w-[486px] animate-fadeIn mx-auto my-3 flex flex-col items-center justify-center rounded-md border border-appGrayLight bg-white p-3">
+        <div
+          style={{
+            backgroundImage: `url(${bg.src})`,
+            filter: 'blur(8px)'
+          }}
+          className="w-full h-full bg-cover bg-center bg-no-repeat z-0 absolute left-0 top-0"
+        />
+        <div className="m-auto flex flex-col items-center z-10">
+          <div className="max-w-[486px] animate-fadeIn backdrop-blur-xl flex flex-col items-center justify-center rounded-md border border-appGrayLight p-3">
             <Image priority src={Logo} width={200} height={200} alt="Logo makaDev" className="my-3" />
             <p className="text-center text-lg font-semibold">{t('page.home.greetings')}</p>
             <p className="text-center">{t('page.home.technologies')}</p>
@@ -39,10 +47,10 @@ const Home = async ({ params: { lng } }: lngProps) => {
 export default Home;
 
 //-----------------------------------------------
-//TODO: improvement UI - home page - add animation background
 //TODO: improvement UI - layout products page and mobile version view
 //TODO: improvement UI - add custom styles to dropdown
 //TODO: improvement UI - add custom styles to checkbox
+//TODO: improvement UI - add background in products and cart tabs
 //TODO: improvement UI - cart tab
 //-----------------------------------------------
 //TODO: add price range filters
@@ -52,4 +60,5 @@ export default Home;
 //-----------------------------------------------
 //TODO: add terms and cookies
 //TODO: add log in/out
+//TODO: improvement UI - home tab - animate background
 //-----------------------------------------------
